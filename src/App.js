8a478f46d256d936a7309e4d9deea84b549face5
geochronology <54387644+geochronology@users.noreply.github.com>
2020-11-10 17:01:46 -0800

    rendering stringified json from mirage server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css"
+export default function App() {
+  const [data, setData] = React.useState(null);
+  React.useEffect(() => {
+    fetch('/api/movies')
+      .then((x) => x.json())
+      .then(setData);
+  }, []);
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <div>
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { Server } from 'miragejs';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+new Server({
+  routes() {
+    this.namespace = 'api';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    this.get('/movies', () => {
+      return [
+        { id: 1, name: 'Inception', year: 2010 },
+        { id: 2, name: 'Interstellar', year: 2014 },
+        { id: 3, name: 'Dunkirk', year: 2017 }
+      ]
+    });
+  }
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
