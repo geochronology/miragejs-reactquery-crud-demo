12aaa3a81a4d7849fd52bb745a63ce9c040163b8
geochronology <54387644+geochronology@users.noreply.github.com>
2020-11-10 17:11:10 -0800

    integrated react-query and moving to feat branch

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import "./App.css"
-export default function App() {
-  const [data, setData] = React.useState(null);
-  React.useEffect(() => {
-    fetch('/api/movies')
-      .then((x) => x.json())
-      .then(setData);
-  }, []);
 
+export default function App() {
+  const { status, data, error } = useQuery('movies', () =>
+    fetch('/api/movies').then((x) => x.json())
+  );
   return (
     <div className="App-header">
+      <div>{status}</div>
+      {error && <div>{error}</div>}
       <div>
-        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+        {status === 'loading' ? (
+          <span>Loading...</span>
+        ) : status === 'error' ? (
+          <span>Error: {error.message}</span>
+        ) : (
+              <ul>
+                {data.map((movie) => (
+                  <li key={movie.id}>
+                    {movie.name} ({movie.year})
+                  </li>
+                ))}
+              </ul>
+            )}
       </div>
     </div>
   );
 }
+
